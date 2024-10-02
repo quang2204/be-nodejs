cca911bc56b6ec641b72d583e228bfddcfd6f714
name22334 <quang20042204@gmail.com>
2024-10-02 10:50:08 +0700

    confic

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 export const connectDb = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/node");
+    await mongoose.connect("mongodb+srv://quanglt22:quang2204@cluster0.4czbvw0.mongodb.net/products").then();
   } catch (error) {
-    console.error(error);
+    console.log(error);
   }
 };
