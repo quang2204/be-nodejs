77341a430ead6fb5b92fda8212951afe42dc32ce
name22334 <quang20042204@gmail.com>
2024-11-08 09:57:20 +0700

    update statu detailuser

--- src/controller/user-joi.js
+++ src/controller/user-joi.js
@@ -113,7 +113,7 @@ export const DeleteUser = async (req, res) => {
 export const DetailUser = async (req, res) => {
   try {
     const data = await User.findById(req.params.id);
-    return res.status(201).json(data);
+    return res.status(200).json(data);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
