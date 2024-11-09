eb99ba34fd93aeec9e35c29f65d895dafe788cb4
name22334 <quang20042204@gmail.com>
2024-11-09 11:02:53 +0700

    fix router

--- src/router/order.js
+++ src/router/order.js
@@ -13,6 +13,6 @@ router.get("/orders", GetOrder);
 router.post("/order", AddOrder);
 router.patch("/order/:id", UpdateOrder);
 router.delete("/order/:id", DeleteOrder);
-router.get("/order/:userid", GetOrderByUser);
+router.get("/order/user/:userid", GetOrderByUser);
 router.get("/order/:id", DetailOrder);
 export default router;
