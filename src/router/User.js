7c4af6c957f2d1186f4cd1a0b19099404ccd220b
quangluongw <quang20042204@gmail.com>
2025-05-23 10:09:40 +0700

    token

--- src/router/User.js
+++ src/router/User.js
@@ -1,18 +1,18 @@
 import express from "express";
 
 import {
-  singin,
   singup,
   updateUser,
   DeleteUser,
   GetUser,
   DetailUser,
   UpdatePassword,
+  signin,
 } from "../controller/user-joi";
 const router = express.Router();
 router.post("/register", singup);
 router.get("/user", GetUser);
-router.post("/login", singin);
+router.post("/login", signin);
 router.patch("/user/:id", updateUser);
 router.delete("/user/:id", DeleteUser);
 router.get("/user/:id", DetailUser);
